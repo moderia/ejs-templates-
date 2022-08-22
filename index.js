d5648d921e65c4b33b820dc9dc6b785a308d913f
Mohamed Deria <103150230+moderia@users.noreply.github.com>
2022-08-22 02:06:09 +0000

    new update

--- index.js
+++ index.js
@@ -1,5 +1,50 @@
-var http = require("http");
-http.createServer(function(req,res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Aloha world');
-}).listen(process.env.PORT || 3000);
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.set('view engine','ejs')
+
+
+//this will allow us to serve up static files, CSS, images & JS
+app.use(express.static(__dirname));
+
+
+app.get('/', (req, res) => {
+  var title = "Home"
+  var heading = "Mohameds Favorite Things"
+  res.render('pages/index',{
+    'title':title,
+    'heading':heading
+  })
+})
+
+app.get('/Basketball', (req, res) => {
+  var title = "Mohameds Favorite basketball teams is the Warriors"
+  var heading = 'My Website'
+  res.render('pages/basketball',{
+    'title':title,
+    'heading':heading
+  })
+})
+
+app.get('/music', (req, res) => {
+  var title = "My favorite album to drop this year was love, Damini"
+  var heading = 'My Website'
+  res.render('pages/music',{
+    'title':title,
+    'heading':heading
+  })
+})
+
+app.get('/artist', (req, res) => {
+  var title = "Favorite artist right now is Burna Boy"
+  var heading = 'My Website'
+  res.render('pages/artist',{
+    'title':title,
+    'heading':heading
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
